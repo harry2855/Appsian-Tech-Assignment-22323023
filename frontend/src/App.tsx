9bba134b2e8dda398a60da9c2e967a8f242abdcc
Himanshu Raheja <harry@Himanshus-MacBook-Air.local>
2025-10-26 08:48:14 +0530

    updated_code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './hook/hook';
 import ProtectedRoute from "./components/Structure/strucutre";
