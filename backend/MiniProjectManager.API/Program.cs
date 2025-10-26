58cf40587bf00be0b7e70eed89c5d96c82970c5b
Himanshu Raheja <harry@Himanshus-MacBook-Air.local>
2025-10-26 09:09:14 +0530

    CORS_error

--- backend/MiniProjectManager.API/Program.cs
+++ backend/MiniProjectManager.API/Program.cs
@@ -103,7 +103,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors("AllowFrontend");
+app.UseCors("AllowReactApp");
 
 app.UseAuthentication();
 app.UseAuthorization();
